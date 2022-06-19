c2ebdcc6603bf5072db898f4555d67cf919c8d71
Prashant Surwase <Prashantsurwase111@gmail.com>
2022-06-20 00:47:47 +0530

    files added

---
+++ demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+declare -a arr
+
+num1=$((RANDOM%900+100));
+echo "First three digit num : "$num1
+num2=$((RANDOM%900+100));
+echo "Second three digit num : "$num2
+num3=$((RANDOM%900+100));
+echo "Third three digit num : "$num3
+num4=$((RANDOM%900+100));
+echo "fourth three digit num : "$num4
+num5=$((RANDOM%900+100));
+echo "fifth three digit num : "$num5
+
